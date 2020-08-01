758212b63904a255e1e27f0f9730b0b5d4ed0190
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-01 22:32:45 +0530

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,30 +1,61 @@
-const express = require("express");
-const app = express();
-const { resolve } = require("path");
-// This is your real test secret API key.
-const stripe = require("stripe")("sk_test_51HAbz2AQ0qxfxSJ1jggW2kn1pxJB6891cSZk9AkwvlOiv2y6JMonxPHaVXgz6cQHAxhOTn7GDkyAqLNJKCD8oClP00NqDqfZeD");
 
-app.use(express.static("."));
-app.use(express.json());
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-03-02',
+  maxNetworkRetries: 2,
+});
+
+
 
 const calculateOrderAmount = items => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1400;
+  return 15;
 };
 
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-  // Create a PaymentIntent with the order amount and currency
+
+
+/*
+ * Product data can be loaded from anywhere. In this case, we’re loading it from
+ * a local JSON file, but this could also come from an async call to your
+ * inventory management service, a database query, or some other API call.
+ *
+ * The important thing is that the product info is loaded from somewhere trusted
+ * so you know the pricing information is accurate.
+ */
+
+
+
+const inventory = require('./data/products.json');
+
+
+
+exports.handler = async (event) => {
+
+  // const { sku, quantity } = JSON.parse(event.body);
+
+  // const product = inventory.find((p) => p.sku === sku);
+
+  // const validatedQuantity = quantity > 0 && quantity < 11 ? quantity : 1;
+
+
+
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
     currency: "usd"
   });
 
-  res.send({
-    clientSecret: paymentIntent.client_secret
-  });
-});
 
-app.listen(4242, () => console.log('Node server listening on port 4242!'));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      //sessionId: paymentIntent.id,
+      clientSecret: paymentIntent.client_secret,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    }),
+  };
+
+  
+  
+};
