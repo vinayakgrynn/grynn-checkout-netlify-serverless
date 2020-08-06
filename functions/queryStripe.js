16a63fdb49bdf9c7dfdff624b92653d588f6446c
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-06 14:31:53 +0530

    Update queryStripe.js

--- functions/queryStripe.js
+++ functions/queryStripe.js
@@ -16,7 +16,7 @@ stripe.customers.list(
     return {
             statusCode: 200,
             headers: {"Access-Control-Allow-Origin":"*"},
-            body: JSON.stringify(customers),
+            body: customers
         };
     
   }
