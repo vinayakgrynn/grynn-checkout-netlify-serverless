0d6d4557874086336100f4a0d71c40e58544ea02
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-10 20:26:54 +0530

    Update retrieveCustomer.js

--- functions/retrieveCustomer.js
+++ functions/retrieveCustomer.js
@@ -5,37 +5,36 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
 });
 
 
-//const inventory = require('./data/products.json');
-
-
-exports.handler = async (event, context, callback) => { //= async (event) =>
+exports.handler = async (event, context, callback) => { 
 
 const datav = JSON.parse(event.body);
 
-const data = JSON.parse(datav);
+console.log(datav);
+  
+//const data = JSON.parse(datav);
   
 console.log(data);
   
 //const address = data[0];
 /*
-const cust = await stripe.customers.retrieve(
-  address.customerID , 
-  function(err, customer) {
-    // asynchronously called
-  }
-);
+  const cust = await stripe.customers.retrieve(
+    address.customerID , 
+    function(err, customer) {
+      // asynchronously called
+    }
+  );
 
 
-console.log("customer: ", cust ); 
+  console.log("customer: ", cust ); 
   
-return {
+  return {
     statusCode: 200,
     headers: {"Access-Control-Allow-Origin":"*"},
     body: JSON.stringify({
       customerID: cust.id , 
       Customer: cust,
     }),
-};
+  };
 */
   
 };
