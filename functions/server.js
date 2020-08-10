7d943fd2de8158b9f92690e60a4e1ee0e0888460
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-10 11:50:01 +0530

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -69,6 +69,36 @@ const paymentIntent = await stripe.paymentIntents.create({
   
 console.log("paymentIntent: ", paymentIntent );
   
+  
+const cust = await stripe.customers.create({
+  name: address.firstname + " " + address.lastname ,
+  phone: address.tel,
+  email: address.email,
+  address: {
+    line1: address.billaddress + "  " + address.billaddress2 ,
+    postal_code: address.billpostcode ,
+    city: address.billcity ,
+    state: '',
+    country: address.billcountry,
+  },
+  shipping: {
+      name: address.firstname + " " + address.lastname ,
+      phone: address.tel,
+      address: {
+        line1: address.shipaddress + "  " + address.shipaddress2 ,
+        postal_code: address.shippostcode ,
+        city: address.shipcity ,
+        state: '',
+        country: address.shipcountry,
+      },
+  },
+  
+});
+  
+
+console.log("customer: ", cust );
+  
+  
 return {
     statusCode: 200,
     headers: {"Access-Control-Allow-Origin":"*"},
