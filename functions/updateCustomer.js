211d49e0e0a123e153aeeaa2c2c9670137ff8424
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-10 19:02:36 +0530

    Update updateCustomer.js

--- functions/updateCustomer.js
+++ functions/updateCustomer.js
@@ -17,27 +17,6 @@ const data = JSON.parse(datav);
 console.log(data);
   
 const address = data[0];
-const cart = data[1];
-const len = cart.length;
-
-
-var totalCount = 0.0;
-var totalCart = 0.0;
-
-//console.log(typeof(cart), len, cart);
-
-for (i = 0; i < len; i++) {
-  console.log("Cart: ", cart[i]);
-  totalCount += cart[i].count ;
-  totalCart += parseFloat( cart[i].total );
-}
-
-console.log("totalCount,  totalCart: ", totalCount, totalCart);
-
-var vtotal = (totalCart).toFixed(2);
-totalCart = parseInt(vtotal * 100);
-console.log("totalCount,  totalCart: ", totalCount, totalCart);
-
 
 
 const cust = await stripe.customers.update(
@@ -70,8 +49,6 @@ const cust = await stripe.customers.update(
 
 
 console.log("customer: ", cust );
-  
-console.log("paymentIntent: ", paymentIntent );
   
 return {
     statusCode: 200,
