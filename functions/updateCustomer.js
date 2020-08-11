52d7742e739d508b396ab6245defd52d6dc3de93
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-11 15:04:40 +0530

    Update updateCustomer.js

--- functions/updateCustomer.js
+++ functions/updateCustomer.js
@@ -5,11 +5,10 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
 });
 
 
-//const inventory = require('./data/products.json');
-
 
 exports.handler = async (event, context, callback) => { //= async (event) =>
 
+  
 const datav = JSON.parse(event.body);
 
 const data = JSON.parse(datav);
@@ -17,34 +16,53 @@ const data = JSON.parse(datav);
 console.log(data);
   
 const address = data[0];
+const cart = data[1];
+const customerID = data[2];
+const len = cart.length;
+
+console.log("\nJSON:\n", typeof(address), address["firstname"] , address.firstname);
+console.log("\naddress\n",typeof(address), address);
+console.log("\ncart\n", typeof(cart), cart);
 
 
-const cust = await stripe.customers.update(
-  address.customerID ,
-  {
-  name: address.firstname + " " + address.lastname ,
-  phone: address.tel,
-  email: address.email,
-  address: {
-    line1: address.billaddress + "  " + address.billaddress2 ,
-    postal_code: address.billpostcode ,
-    city: address.billcity ,
-    state: '',
-    country: address.billcountry,
-  },
-  shipping: {
-      name: address.firstname + " " + address.lastname ,
-      phone: address.tel,
-      address: {
-        line1: address.shipaddress + "  " + address.shipaddress2 ,
-        postal_code: address.shippostcode ,
-        city: address.shipcity ,
-        state: '',
-        country: address.shipcountry,
-      },
-  },
+var totalCount = 0.0;
+var totalCart = 0.0;
+
+
   
-  });
+const cust = await stripe.customers.update(
+  
+  customerID ,
+  
+  {
+    
+    name: address.firstname + " " + address.lastname ,
+    phone: address.tel,
+    email: address.email,
+    address: {
+      line1: address.billaddress ,
+      line2: address.billaddress2 ,
+      postal_code: address.billpostcode ,
+      city: address.billcity ,
+      state: '',
+      country: address.billcountry,
+    },
+    shipping: {
+        name: address.firstname + " " + address.lastname ,
+        phone: address.tel,
+        address: {
+          line1: address.shipaddress ,
+          line2: address.shipaddress2 ,
+          postal_code: address.shippostcode ,
+          city: address.shipcity ,
+          state: '',
+          country: address.shipcountry,
+        },
+    },
+    
+  }
+  
+);
   
 
 
