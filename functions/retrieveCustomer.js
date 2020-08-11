a32d33a228a907c90dc06b0b43c0bc9a71caa8c3
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-11 13:27:57 +0530

    Update retrieveCustomer.js

--- functions/retrieveCustomer.js
+++ functions/retrieveCustomer.js
@@ -16,20 +16,14 @@ exports.handler = async (event, context, callback) => {
 
   const cust = await stripe.customers.retrieve(
     customerID , 
-    function(err, customer) {
-      // asynchronously called
-    }
   );
-
   
   console.log("customer: ", cust ); 
-  
   
   return {
     statusCode: 200,
     headers: {"Access-Control-Allow-Origin":"*"},
     body: JSON.stringify({
-      customerID: cust.id , 
       Customer: cust,
     }),
   };
