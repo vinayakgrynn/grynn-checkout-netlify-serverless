385f1c96bfb70221dd31a91a6657b4b6ec9975cd
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-08-12 19:33:15 +0530

    Update retrieveCustomer.js

--- functions/retrieveCustomer.js
+++ functions/retrieveCustomer.js
@@ -10,22 +10,47 @@ exports.handler = async (event, context, callback) => {
   const data = JSON.parse(event.body);
 
   console.log(data);
+  
   console.log("typeof: ", typeof(data));
  
-  const customerID = data;
+  const customerID = data.key_value;
+  
+  var netlify_access_token = data["netlify_access_token"];
+  
+  var queryData = data;
+  
+  var uuser = context.clientContext.user.sub;
+  
+  if( uuser.localeCompare(queryData.title) === 0 )
+  {
+    
+      console.log( " uuser === queryData.title ",  uuser, queryData.title );
+    
+      const cust = await stripe.customers.retrieve(
+        customerID , 
+      );
+  
+      console.log("customer: ", cust ); 
+  
+      return {
+        statusCode: 200,
+        headers: {"Access-Control-Allow-Origin":"*"},
+        body: JSON.stringify( { Customer: cust } )
+      }
+    
+  }
+  else
+  {    
+      console.log( " uuser !== queryData.title ",  uuser, queryData.title );
+      
+      return {
+          headers: {"Access-Control-Allow-Origin":"*"},
+          statusCode: 401,
+          body: JSON.stringify("Unauthorized: Bad Token")
+      }
 
-  const cust = await stripe.customers.retrieve(
-    customerID , 
-  );
-  
-  console.log("customer: ", cust ); 
-  
-  return {
-    statusCode: 200,
-    headers: {"Access-Control-Allow-Origin":"*"},
-    body: JSON.stringify({
-      Customer: cust,
-    }),
-  };
+  }
+    
+    
   
 };
